316e2a29f5a1216428073f2bc21b05f6cac6f540
Ed Audland <edaudland@gmail.com>
2025-03-04 13:03:42 +0000

    Basic Navbar built

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    # For additional fields in app/views/devise/registrations/new.html.erb
+    # For additional parameters in User registration and edit
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name location])
-
-    # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name location])
   end
 end
