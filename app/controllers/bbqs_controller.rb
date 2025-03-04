5da0ca1d74ae174c3a31839263484a4b5776a4cc
[B[B[B[B[B[B[B[B[B[B[B[B <tomwparkinson@hotmail.co.uk>
2025-03-04 16:40:30 +0000

    initial code

--- app/controllers/bbqs_controller.rb
+++ app/controllers/bbqs_controller.rb
@@ -4,6 +4,19 @@ class BbqsController < ApplicationController
 
   def index
     @bbq = Bbq.all
+
+    # adding in code to filter bbqs based on user dates
+    if params[:start_date].present? && params[:end_date].present?
+      start_date = Date.parse(params[:start_date])
+      end_date = Date.parse(params[:end_date])
+
+      # Assuming a Barbecue has many bookings with a `start_date` and `end_date`
+      @bbqs = @bbqs.reject do |bbq|
+        bbq.bookings.exists?(
+          ["(start_date, end_date) OVERLAPS (?, ?)", start_date, end_date]
+        )
+      end
+    end
   end
 
   def show
