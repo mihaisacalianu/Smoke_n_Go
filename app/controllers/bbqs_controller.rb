4fb4c8fb13460d606c8a85ffe29ddefc96209137
[B[B[B[B[B[B[B[B[B[B[B[B <tomwparkinson@hotmail.co.uk>
2025-03-06 16:55:46 +0000

    more developed search bar

--- app/controllers/bbqs_controller.rb
+++ app/controllers/bbqs_controller.rb
@@ -5,14 +5,7 @@ class BbqsController < ApplicationController
   def index
     @bbqs = Bbq.all
 
-    # if params[:filters]
-    # @bbqs = @bbqs.where(brand: params[:filters][:brand]) if params[:filters][:brand].present?
-    # @bbqs = @bbqs.where(fuel_type: params[:filters][:fuel_type]) if params[:filters][:fuel_type].present?
-    # @bbqs = @bbqs.where(grill_size: params[:filters][:grill_size]) if params[:filters][:grill_size].present?
-    # @bbqs = @bbqs.where(pick_up: true) if params[:filters][:pick_up] == "1"
-    # @bbqs = @bbqs.where(delivery: true) if params[:filters][:delivery] == "1"
-    # @bbqs = @bbqs.where("price <= ?", params[:filters][:price]) if params[:filters][:price].present?
-    # end
+    @start_date, @end_date = extract_dates(params)
 
     @markers = @bbqs.geocoded.map do |bbq|
       {
@@ -23,18 +16,15 @@ class BbqsController < ApplicationController
       }
     end
 
-
-    if params["booking"][:start_date].present? && params["booking"][:end_date].present?
-      start_date = Date.parse(params["booking"][:start_date])
-      end_date = Date.parse(params["booking"][:end_date])
-
-      # Assuming a Barbecue has many bookings with a `start_date` and `end_date`
-      @bbqs = @bbqs.reject do |bbq|
-        bbq.bookings.exists?(
-          ["(start_date, end_date) OVERLAPS (?, ?)", start_date, end_date]
-        )
-      end
+    if params[:filters]
+      @bbqs = @bbqs.where(brand: params[:filters][:brand]) if params[:filters][:brand].present?
+      @bbqs = @bbqs.where(fuel_type: params[:filters][:fuel_type]) if params[:filters][:fuel_type].present?
+      @bbqs = @bbqs.where(grill_size: params[:filters][:grill_size]) if params[:filters][:grill_size].present?
+      @bbqs = @bbqs.where(pick_up: true) if params[:filters][:pick_up] == "1"
+      @bbqs = @bbqs.where(delivery: true) if params[:filters][:delivery] == "1"
+      @bbqs = @bbqs.where("price <= ?", params[:filters][:price]) if params[:filters][:price].present?
     end
+
   end
 
   def show
@@ -95,5 +85,18 @@ class BbqsController < ApplicationController
 
   def bbq_params
     params.require(:bbq).permit(:name, :description, :address, :price, :brand, :fuel_type, :grill_size, :pick_up, :delivery, :dates_unavailable, :photo)
+  end
+
+  def extract_dates(params)
+    if params[:start_date].present? && params[:end_date].present?
+      start_date = Date.parse(params[:start_date])
+      end_date = Date.parse(params[:end_date])
+    elsif params.dig(:booking, :start_date).present? && params.dig(:booking, :end_date).present?
+      start_date = Date.parse(params[:booking][:start_date])
+      end_date = Date.parse(params[:booking][:end_date])
+    end
+    [start_date, end_date]
+  rescue ArgumentError
+    [nil, nil] # Handles invalid date errors
   end
 end
--- app/javascript/controllers/filter_controller.js
+++ app/javascript/controllers/filter_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["filterBar"]
+  static targets = ["filterBar", "searchDates", "reset", "startDate", "endDate"]
 
   connect() {
     console.log("hello from filter controller")
@@ -18,10 +18,17 @@ export default class extends Controller {
   }
 
   submit() {
-    console.log("hello from submit action")
-    // if (this.filterBarTarget.style.display === "none") {
-    //   this.filterBarTarget.style.display = "block";
-    // } else {
-    //   this.filterBarTarget.style.display = "none";
+    this.formTarget.submit();
+  }
+
+  reset(event) {
+    event.preventDefault();
+
+    const startDate = this.startDateTarget.value;
+    const endDate = this.endDateTarget.value;
+    this.resetTarget.reset();
+    this.startDateTarget.value = startDate;
+    this.endDateTarget.value = endDate;
+    this.resetTarget.submit();
   }
 }
